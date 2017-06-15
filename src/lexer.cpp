0898dc22629ba5344176e93437d3d1aaab902db3
Giuseppe Lipari <g.lipari@sssup.it>
2017-06-15 16:43:17 +0200

    Now using std::regex instead of boost::regex Removed dependency from Boost Still a boost dependency in one of the example (not compiled), to be removed

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -2,7 +2,8 @@
 #include <iomanip>
 #include "log_macros.hpp"
 
-#include <boost/regex.hpp>
+//#include <boost/regex.hpp>
+#include <regex>
 #include <tipa/lexer.hpp>
 
 using namespace std;
@@ -110,92 +111,93 @@ namespace tipa {
 
     void lexer::skip_spaces()
     {
-	while (start != curr_line.end()) {
-	    int d = std::distance(start, curr_line.end());
-	    int m = std::min((int)comment_begin.size(), d);
-	    int n = std::min((int)comment_single_line.size(), d);
-	    std::string p(start, start+m);
-	    std::string q(start, start+n);
+        while (start != curr_line.end()) {
+            int d = std::distance(start, curr_line.end());
+            int m = std::min((int)comment_begin.size(), d);
+            int n = std::min((int)comment_single_line.size(), d);
+            std::string p(start, start+m);
+            std::string q(start, start+n);
 	
-	    if (*start == ' ' or *start == '\n') {
-		++start; ++ncol;
-	    } else if (*start == '\t') {
-		++start; ncol += 8;
-	    } else if (m != 0 and comment_begin == p) {
-		start += m;
-		extract(comment_begin, comment_end);
-	    } 
-	    else if (n != 0 and comment_single_line == q)
-		extract_line();
-	    else break;
-	}
+            if (*start == ' ' or *start == '\n') {
+                ++start; ++ncol;
+            } else if (*start == '\t') {
+                ++start; ncol += 8;
+            } else if (m != 0 and comment_begin == p) {
+                start += m;
+                extract(comment_begin, comment_end);
+            } 
+            else if (n != 0 and comment_single_line == q)
+                extract_line();
+            else break;
+        }
     }
 
 
     token_val lexer::try_token(const token &x)
     {
-	static boost::match_results<std::string::iterator> what;
-	skip_spaces(); 
+        //static boost::match_results<std::string::iterator> what;
+        static std::match_results<std::string::iterator> what;
+        skip_spaces(); 
 
-	while (start == curr_line.end() or *start == 0) {
-	    if (not next_line()) return { LEX_ERROR, "EOF" };
-	    skip_spaces();
-	}
+        while (start == curr_line.end() or *start == 0) {
+            if (not next_line()) return { LEX_ERROR, "EOF" };
+            skip_spaces();
+        }
 
-	INFO_LINE("try_token(): start is at <" << *start << ">");
+        INFO_LINE("try_token(): start is at <" << *start << ">");
 
-	INFO_LINE(curr_line);
-	INFO_LINE(std::setw(ncol) << "^");
+        INFO_LINE(curr_line);
+        INFO_LINE(std::setw(ncol) << "^");
 
-	boost::regex expr(x.get_expr());
-	auto flag = boost::regex_search(start, curr_line.end(), what, expr, 
-					boost::match_continuous);
+        std::regex expr(x.get_expr());
+        auto flag = std::regex_search(start, curr_line.end(), what, expr, 
+                                      std::regex_constants::match_continuous);
 
-	INFO_LINE("Regex_search completed");
-	if (flag) {
-	    string res;
-	    copy(start, what[0].second, back_inserter(res));
-	    ncol += distance(start, what[0].second);
-	    start = what[0].second;
-	    return token_val(x.get_name(), res);
-	}
-	INFO_LINE("Token does not match");
-	return { LEX_ERROR, "Token does not match" };
+        INFO_LINE("Regex_search completed");
+        if (flag) {
+            string res;
+            copy(start, what[0].second, back_inserter(res));
+            ncol += distance(start, what[0].second);
+            start = what[0].second;
+            return token_val(x.get_name(), res);
+        }
+        INFO_LINE("Token does not match");
+        return { LEX_ERROR, "Token does not match" };
     }
 
     std::pair<token_id, std::string> ahead_lexer::get_token()
     {
-	static boost::match_results<std::string::iterator> what;
+        static std::match_results<std::string::iterator> what;
 
-	skip_spaces(); 
+        skip_spaces(); 
 
-	while (start == curr_line.end() or *start == 0) {
-	    if (not next_line()) return { LEX_ERROR, "EOF" };
-	    skip_spaces();
-	}
+        while (start == curr_line.end() or *start == 0) {
+            if (not next_line()) return { LEX_ERROR, "EOF" };
+            skip_spaces();
+        }
 
-	INFO_LINE("get_token(): start is at \"" << *start << "\"");
-	INFO_LINE(curr_line);
-	INFO_LINE(std::setw(ncol) << "^");
+        INFO_LINE("get_token(): start is at \"" << *start << "\"");
+        INFO_LINE(curr_line);
+        INFO_LINE(std::setw(ncol) << "^");
 
-	// try to identify which token
-	for (auto x : array) {
-	    boost::regex expr(x.get_expr());
-	    auto flag = boost::regex_search(start, curr_line.end(), what, expr, 
-					    boost::match_continuous);
-	    if (flag) {
-		string res;
-		copy(start, what[0].second, back_inserter(res));
-		ncol += distance(start, what[0].second);
-		start = what[0].second;
-		// INFO_LINE("get_token(): start now pointing at \"" << *start << "\"");
-		// INFO_LINE("token: " << res);
-		return {x.get_name(), res};
-		break;
-	    }
-	}
-	INFO_LINE("token not found");
-	return { LEX_ERROR, "Unknown token" };
+        // try to identify which token
+        for (auto x : array) {
+            std::regex expr(x.get_expr());
+            auto flag = std::regex_search(start, curr_line.end(), what, expr, 
+                                          std::regex_constants::match_continuous);
+            if (flag) {
+                string res;
+                copy(start, what[0].second, back_inserter(res));
+                ncol += distance(start, what[0].second);
+                start = what[0].second;
+                // INFO_LINE("get_token(): start now pointing at \"" << *start << "\"");
+                // INFO_LINE("token: " << res);
+                return {x.get_name(), res};
+                break;
+            }
+        }
+        INFO_LINE("token not found");
+        return { LEX_ERROR, "Unknown token" };
     }
 
     std::string lexer::extract_line()
